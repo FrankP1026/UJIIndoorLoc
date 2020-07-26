455c81e29e69bc25e163539b9029e8630d2219cb
Frank Pan <frank@aislelabs.com>
2020-07-26 18:23:01 -0400

    first commit with data transformation

---
+++ main.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+NUM_SENSORS = 520
+SENSOR_COLUMN_NAME_PREFIX = 'WAP'
+
+def get_sensor_column_name(sensor_index):
+    return SENSOR_COLUMN_NAME_PREFIX + "{:03d}".format(sensor_index)
+
+def transform_signal_strength(original_value):
+    return 0 if original_value == 100 else 100 + original_value
+
+def get_all_sensor_column_names():
+    column_names = []
+    for i in range(1, NUM_SENSORS + 1):
+        column_name = get_sensor_column_name(i)
+        column_names.append(column_name)
+
+    return column_names
+
+training_data = pd.read_csv('./trainingData.csv')
+print(training_data.shape)
+print(training_data.head())
+
+sensor_columns = get_all_sensor_column_names()
+print(training_data.loc[:, sensor_columns].head())
+
+training_data.loc[:, sensor_columns] = training_data.loc[:, sensor_columns].applymap(transform_signal_strength)
+print(training_data.head())
