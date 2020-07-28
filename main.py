386dd472cdabbce0be0f532ab37cd3af500ab8e6
Frank Pan <frank@aislelabs.com>
2020-07-28 01:49:39 -0400

    able to build a model and achieve good accuracy for the first try

--- main.py
+++ main.py
@@ -29,11 +29,46 @@ def read_and_preprocess_data(file_dir):
 
     # Categorize each row based on Building ID and Floor?
     data['position'] = data.apply(
-        lambda row : str(int(row['BUILDINGID'])) + "_" + str(int(row['FLOOR'])), axis='columns').astype('category')
-    print(data.head())
+        lambda row : str(int(row['BUILDINGID'])) + "_" + str(int(row['FLOOR'])), axis='columns')
+    data['position'] = data['position'].astype('category')
     return data
+
+def get_number_of_classes(training_data_positions_coded):
+    training_data_positions_coded.nunique()
+
+def extract_features_and_targets(data_frame, num_classes=None):
+    sensor_columns = get_all_sensor_column_names()
+    positions_coded = data_frame['position'].cat.codes
+    number_of_classes = num_classes if num_classes is not None else positions_coded.nunique()
+
+    position_categorical = tf.keras.utils.to_categorical(
+        positions_coded.to_numpy(), num_classes=number_of_classes
+    )
+
+    return (data_frame.loc[:, sensor_columns].to_numpy(), position_categorical)
 
 if __name__ == '__main__':
     training_data = read_and_preprocess_data('./trainingData.csv')
-# TODO: feed the training data to a neural network, try to tune it to get a good performance
-#   for both the training and test dataset
+
+    number_of_classes = training_data['position'].cat.codes.nunique()
+    features, targets = extract_features_and_targets(training_data, num_classes=number_of_classes)
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(50, activation='relu'),
+        tf.keras.layers.Dense(50, activation='relu'),
+        tf.keras.layers.Dense(number_of_classes, activation='softmax')
+    ])
+
+    model.compile(
+        optimizer='adam',
+        loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+        metrics=['accuracy']
+    )
+
+    model.fit(x=features, y=targets, epochs=100)
+
+    # testing
+    test_data = read_and_preprocess_data('validationData.csv')
+    x_test, y_test = extract_features_and_targets(test_data, num_classes=number_of_classes)
+    accr = model.evaluate(x_test, y_test)
+    print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0],accr[1]))
